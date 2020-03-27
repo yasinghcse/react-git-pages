deff474d0790b8788178bb588ef10f934dad64de
Yadwinder <yadwinder.singh11@gmail.com>
2020-03-26 21:21:19 -0400

    Updates

---
+++ precache-manifest.25f99c3341fd62e3f78be836d683d89c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac69a764ebb1fe0129c6773360a0ae6",
+    "url": "/react-git-pages/index.html"
+  },
+  {
+    "revision": "936a82ced127b2cf7b02",
+    "url": "/react-git-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e06d3f8ba85df0b95d1",
+    "url": "/react-git-pages/static/js/2.b8b3d245.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-git-pages/static/js/2.b8b3d245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936a82ced127b2cf7b02",
+    "url": "/react-git-pages/static/js/main.4cd5de0f.chunk.js"
+  },
+  {
+    "revision": "3e8e187e94e70411bec4",
+    "url": "/react-git-pages/static/js/runtime-main.214f1da3.js"
+  }
+]);
